5af33a4beeb7a523a9c9d22d7239d691fb731ba1
abdi <abdtsegaye@gmail.com>
2022-07-31 20:37:17 +0300

    made swiping action

--- lib/Swipe/swipe_screen.dart
+++ lib/Swipe/swipe_screen.dart
@@ -3,6 +3,7 @@
 import 'package:dating_app/Common/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:icons_plus/icons_plus.dart';
+import 'package:swipe_cards/swipe_cards.dart';
 
 class SwipeScreen extends StatefulWidget {
   static String id = 'SwipeScreen';
@@ -14,6 +15,70 @@ class SwipeScreen extends StatefulWidget {
 }
 
 class _SwipeScreenState extends State<SwipeScreen> {
+  final List<SwipeItem> _swipeItems = <SwipeItem>[];
+  late MatchEngine _matchEngine;
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+  final List<String> _names = [
+    "Red",
+    "Blue",
+    "Green",
+    "Yellow",
+    "Orange",
+  ]; // TODO: replace this line when implementing data from backend
+
+  @override
+  void initState() {
+    for (int i = 0; i < _names.length; i++) {
+      _swipeItems.add(
+        SwipeItem(
+          content: Candidate(
+            name: 'Semira',
+            age: 23,
+            image: 'assets/images/semira.png',
+            bio:
+                'Some bio text Some bio text Some bio text Some bio text Some bio text ',
+            city: 'Harar',
+            country: 'Ethiopia',
+            zodiac: 'Aquarius',
+            onLike: () => _matchEngine.currentItem!.like(),
+            onDisLike: () => _matchEngine.currentItem!.nope(),
+            onSuperLike: () => _matchEngine.currentItem!.superLike(),
+          ),
+          likeAction: () {
+            _scaffoldKey.currentState!.showSnackBar(
+              SnackBar(
+                content: Text("Liked ${_names[i]}"),
+                duration: const Duration(milliseconds: 500),
+              ),
+            );
+          },
+          nopeAction: () {
+            _scaffoldKey.currentState!.showSnackBar(
+              SnackBar(
+                content: Text("Nope ${_names[i]}"),
+                duration: const Duration(milliseconds: 500),
+              ),
+            );
+          },
+          superlikeAction: () {
+            _scaffoldKey.currentState!.showSnackBar(
+              SnackBar(
+                content: Text("Super liked ${_names[i]}"),
+                duration: const Duration(milliseconds: 500),
+              ),
+            );
+          },
+          onSlideUpdate: (region) async {
+            debugPrint("Region $region");
+          },
+        ),
+      );
+    }
+
+    _matchEngine = MatchEngine(swipeItems: _swipeItems);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -37,191 +102,250 @@ class _SwipeScreenState extends State<SwipeScreen> {
             ),
           ),
           Expanded(
+            child: SwipeCards(
+              matchEngine: _matchEngine,
+              itemBuilder: (BuildContext context, int index) {
+                return _swipeItems[index].content;
+              },
+              onStackFinished: () {
+                _scaffoldKey.currentState?.showSnackBar(const SnackBar(
+                  content: Text("Stack Finished"),
+                  duration: Duration(milliseconds: 500),
+                ));
+              },
+              itemChanged: (SwipeItem item, int index) {
+                print("item: ${item.content.toString()}, index: $index");
+              },
+              upSwipeAllowed: true,
+              fillSpace: true,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Candidate extends StatelessWidget {
+  static String id = 'Candidate';
+  const Candidate({
+    Key? key,
+    required this.name,
+    required this.age,
+    required this.zodiac,
+    required this.city,
+    required this.country,
+    required this.bio,
+    required this.image,
+    required this.onLike,
+    required this.onDisLike,
+    required this.onSuperLike,
+  }) : super(key: key);
+
+  final String name;
+  final int age;
+  final String zodiac;
+  final String city;
+  final String country;
+  final String bio;
+  final String image;
+
+  final VoidCallback onLike;
+  final VoidCallback onDisLike;
+  final VoidCallback onSuperLike;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(
+        left: 10,
+        right: 10,
+        top: 10,
+        bottom: 100,
+      ),
+      width: double.infinity,
+      clipBehavior: Clip.antiAlias,
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.3),
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
             child: Container(
-              margin: const EdgeInsets.only(
-                left: 10,
-                right: 10,
-                top: 10,
-                bottom: 100,
-              ),
               width: double.infinity,
-              clipBehavior: Clip.antiAlias,
               decoration: BoxDecoration(
-                color: Colors.white.withOpacity(0.3),
                 borderRadius: BorderRadius.circular(15),
               ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    child: Container(
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      child: const Image(
-                        image: AssetImage(
-                          'assets/images/semira.png',
-                        ),
-                        fit: BoxFit.cover,
-                        alignment: Alignment.topCenter,
-                      ),
+              child: Image(
+                image: AssetImage(
+                  image,
+                ),
+                fit: BoxFit.cover,
+                alignment: Alignment.topCenter,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  '$name, $age',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 32,
+                  ),
+                ),
+                const Expanded(child: SizedBox()),
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 6,
+                    horizontal: 10,
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.white.withOpacity(0.3),
+                    ),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Text(
+                    zodiac,
+                    style: const TextStyle(
+                      color: Colors.white,
                     ),
                   ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        const Text(
-                          'Semira, 23',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 32,
-                          ),
-                        ),
-                        const Expanded(child: SizedBox()),
-                        Container(
-                          padding: const EdgeInsets.symmetric(
-                            vertical: 6,
-                            horizontal: 10,
-                          ),
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: Colors.white.withOpacity(0.3),
-                            ),
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                          child: const Text(
-                            'Sagittarius',
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ],
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Text(
+              '$city, $country',
+              style: const TextStyle(
+                color: Colors.black,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Text(
+              bio,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Container(
+                  height: 35,
+                  width: 35,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      color: Colors.black,
                     ),
                   ),
-                  const SizedBox(
-                    height: 10,
+                  child: const Icon(
+                    Icons.undo,
+                    color: Colors.white,
                   ),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 10),
-                    child: Text(
-                      'Harar, Ethiopia',
-                      style: TextStyle(
+                ),
+                GestureDetector(
+                  onTap: onDisLike,
+                  child: Container(
+                    height: 45,
+                    width: 45,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
                         color: Colors.black,
                       ),
                     ),
+                    child: const Icon(
+                      Icons.close,
+                      color: Colors.white,
+                      size: 34,
+                    ),
                   ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 10),
-                    child: Text(
-                      'Harar, Ethiopia Harar, Ethiopia Harar, Ethiopia Harar, Ethiopia Harar, Ethiopia Harar, Ethiopia Harar, Ethiopia Harar, Ethiopia ',
-                      style: TextStyle(
-                        color: Colors.white,
+                ),
+                GestureDetector(
+                  onTap: onSuperLike,
+                  child: Container(
+                    height: 55,
+                    width: 55,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: Colors.black,
                       ),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Container(
-                          height: 35,
-                          width: 35,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            border: Border.all(
-                              color: Colors.black,
-                            ),
-                          ),
-                          child: const Icon(
-                            Icons.undo,
-                            color: Colors.white,
-                          ),
-                        ),
-                        Container(
-                          height: 45,
-                          width: 45,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            border: Border.all(
-                              color: Colors.black,
-                            ),
-                          ),
-                          child: const Icon(
-                            Icons.close,
-                            color: Colors.white,
-                            size: 34,
-                          ),
-                        ),
-                        Container(
-                          height: 55,
-                          width: 55,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            border: Border.all(
-                              color: Colors.black,
-                            ),
-                          ),
-                          child: const Icon(
-                            Icons.star,
-                            color: Colors.amber,
-                            size: 44,
-                          ),
-                        ),
-                        Container(
-                          height: 45,
-                          width: 45,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            border: Border.all(
-                              color: Colors.black,
-                            ),
-                          ),
-                          child: const Icon(
-                            IonIcons.heart,
-                            color: Colors.white,
-                            size: 34,
-                          ),
-                        ),
-                        Container(
-                          height: 35,
-                          width: 35,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            border: Border.all(
-                              color: Colors.black,
-                            ),
-                          ),
-                          child: const Icon(
-                            Icons.ac_unit_sharp,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
+                    child: const Icon(
+                      Icons.star,
+                      color: Colors.amber,
+                      size: 44,
                     ),
                   ),
-                  const SizedBox(
-                    height: 10,
+                ),
+                GestureDetector(
+                  onTap: onLike,
+                  child: Container(
+                    height: 45,
+                    width: 45,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: const Icon(
+                      IonIcons.heart,
+                      color: Colors.white,
+                      size: 34,
+                    ),
                   ),
-                ],
-              ),
+                ),
+                Container(
+                  height: 35,
+                  width: 35,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      color: Colors.black,
+                    ),
+                  ),
+                  child: const Icon(
+                    Icons.ac_unit_sharp,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
             ),
+          ),
+          const SizedBox(
+            height: 10,
           ),
         ],
       ),
